f50bc139a4311fee2794f176c9d1d451e56b90b9
Saran Tanpituckpong <sarunta@gmail.com>
2019-03-17 20:04:55 +0700

    :recycle: Use `gridsome-plugin-ts` instead of manual config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,22 +1,12 @@
 module.exports = {
 	siteName: 'Vue Radio Toggle Buttons',
 	siteDescription: 'Radio toggle buttons for Vue.',
-	chainWebpack(config) {
-		config.resolve.extensions
-			.prepend('.ts');
-
-		config.module
-			.rule('ts')
-				.test(/\.ts$/)
-				.use('ts')
-					.loader('ts-loader')
-					.options({
-						appendTsSuffixTo: [/\.vue$/]
-					});
-	},
 	plugins: [
 		{
 			use: 'gridsome-plugin-pug'
+		},
+		{
+			use: 'gridsome-plugin-ts'
 		}
 	]
 };
