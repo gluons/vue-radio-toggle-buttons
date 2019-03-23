e078957b69b9963a63fbcfc53cf300774de2e913
Saran Tanpituckpong <sarunta@gmail.com>
2019-03-23 17:34:44 +0700

    :wrench: Add production building script

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
 	siteName: 'Vue Radio Toggle Buttons',
 	siteDescription: 'Radio toggle buttons for Vue.',
+	pathPrefix: isProd ? '/vue-radio-toggle-buttons' : '/',
 	chainWebpack(config) {
 		config.module
 			.rule('mjs')
