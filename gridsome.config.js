f6ebdd4887a64d063e8e816cd999da52ad6ae829
Saran Tanpituckpong <sarunta@gmail.com>
2019-04-06 19:15:15 +0700

    :bug: Fix "SyntaxError: Unexpected token" error (workaround)

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,12 @@
+const nodeExternals = require('webpack-node-externals');
+
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
 	siteName: 'Vue Radio Toggle Buttons',
 	siteDescription: 'Radio toggle buttons for Vue.',
 	pathPrefix: isProd ? '/vue-radio-toggle-buttons' : '',
-	chainWebpack(config) {
+	chainWebpack(config, { isServer }) {
 		config.module
 			.rule('mjs')
 			.test(/\.mjs$/)
@@ -12,6 +14,15 @@ module.exports = {
 
 		config.resolve.alias
 			.set('vue$', 'vue/dist/vue.esm.js');
+
+		if (isServer) {
+			config.externals(nodeExternals({
+				whitelist: [
+					/\.css$/,
+					/epic-spinners/
+				]
+			}));
+		}
 	},
 	plugins: [
 		{
